a75a0b68ee0c5df2e9a67fd3ba0dc22d71db513f
shaneferrante <shaneferrante@yahoo.com>
2022-03-23 16:20:29 -0400

    Runner class

---
+++ src/AnimationRunner.java
@@ -0,0 +1,15 @@
+/**
+ * Runner class to hold a main method.
+ */
+public class AnimationRunner {
+
+  /**
+   * The main method of the program.
+   *
+   * @param args the arguments of the program.
+   */
+  public static void main(String[] args) {
+
+  }
+
+}
