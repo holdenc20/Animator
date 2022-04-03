76977224cf78b5dc2a948ae7a27aaf8a4dc028b9
shaneferrante <shaneferrante@yahoo.com>
2022-04-02 22:58:54 -0400

    Main Method

--- src/io/AnimationRunner.java
+++ src/io/AnimationRunner.java
@@ -1,6 +1,14 @@
 package io;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import model.Animator;
+import model.AnimatorState;
 import view.AnimatorView;
+import view.ViewFactory;
 
 /**
  * Runner class to hold a main method.
@@ -14,21 +22,47 @@ public class AnimationRunner {
    */
   public static void main(String[] args) {
     int arg = 0;
-    String out;
-    String in;
-    AnimatorView view;
-    double tickRate;
+    String out = "System.out";
+    String in = "";
+    AnimatorView view = null;
+    double tickRate = 1;
     while (args.length > arg) {
       switch (args[arg]) {
         case "-in":
-          in = args[arg+1];
+          in = args[arg + 1];
+          break;
         case "-out":
-
+          out = args[arg + 1];
+          break;
+        case "-view":
+          view = ViewFactory.makeView(args[arg + 1]);
+          break;
+        case "-speed":
+          tickRate = Double.parseDouble(args[arg + 1]);
+          break;
+        default:
+          throw new IllegalArgumentException("Invalid Command Line Arguments");
       }
       arg += 2;
     }
+    view.setTickRate(tickRate);
+    AnimationFileReader reader = new AnimationFileReader();
+    Animator animator = null;
+    try {
+      animator = reader.readFile(in, new TweenBuilderImpl());
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found");
+      e.printStackTrace();
+    }
+    view.setModelState(animator);
+    try {
+      FileWriter writer = new FileWriter(new File(out));
+      view.setOutput(writer);
+    } catch (IOException e) {
+      System.out.println("IOException");
+      e.printStackTrace();
+    }
   }
-
 
 
 }
